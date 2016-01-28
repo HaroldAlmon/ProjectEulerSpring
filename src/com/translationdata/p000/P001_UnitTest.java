065fbc17d828acbd093ff72931cb525af7034ffa
haroldalmon <HaroldAlmon@users.noreply.github.com>
2016-01-28 11:48:21 -0600

    Call factory to start application

--- src/com/translationdata/p000/P001_UnitTest.java
+++ src/com/translationdata/p000/P001_UnitTest.java
@@ -2,8 +2,6 @@ package com.translationdata.p000;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.translationdata.JUnitTests.FastTest;
 
@@ -14,21 +12,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
 /** Strategy: Spring XML configuration. */
 @Category(FastTest.class)
 public class P001_UnitTest {
-	public P001_MultiplesOf3And5 getMultiplesOf3And5() {
-		ApplicationContext appContext = new ClassPathXmlApplicationContext("com/translationdata/resources/P001_applicationContext.xml");
-		return appContext.getBean("multiplesOf3And5", P001_MultiplesOf3And5.class);
-	}
-
+	String resourcePath;
+	String fileName;
+	final P001_Factory p001_Factory = P001_Factory.getFactory();
+	
 	@Test(timeout=1000)
 	public void SumOfMultiplesOf3And5Below1000() {
-		final int sumOfMultiples = getMultiplesOf3And5().multiplesOf3And5(1_000);
-		System.out.printf("multiplesOf3And5(1000) = %s%n", sumOfMultiples);
+		final int sumOfMultiples = p001_Factory.getMultiplesOf3And5().multiplesOf3And5(1_000);
+		System.out.printf("P001: multiplesOf3And5(1000) = %s%n", sumOfMultiples);
 		assertThat( "Incorrect sum of multiples of 3 or 5 below 1,000", sumOfMultiples, is(233168) );
 	}
 	
 	@Test(timeout=1000)
 	public void SumOfMultiplesOf3And5Below10_000() {
-		final int sumOfMultiples = getMultiplesOf3And5().multiplesOf3And5(10_000);
+		final int sumOfMultiples = p001_Factory.getMultiplesOf3And5().multiplesOf3And5(10_000);
 		System.out.printf("P001: multiplesOf3And5(10_000) = %s%n", sumOfMultiples);
 		assertThat( "Incorrect sum of multiples of 3 or 5 below 10,000", sumOfMultiples, is(23331668) );
 	}
