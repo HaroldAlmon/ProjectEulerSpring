53e8d1f36aa401399635edd108889b4389d85af8
haroldalmon <HaroldAlmon@users.noreply.github.com>
2016-01-28 11:47:38 -0600

    Separate application creation from JUnit logic

---
+++ src/com/translationdata/p000/P001_Factory.java
@@ -0,0 +1,21 @@
+package com.translationdata.p000;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public enum P001_Factory {
+	INSTANCE;
+
+	ApplicationContext appContext;
+	
+	public static P001_Factory getFactory() {
+		return INSTANCE;
+	}
+	
+	public P001_MultiplesOf3And5 getMultiplesOf3And5() {
+		if(appContext == null) {
+			appContext = new ClassPathXmlApplicationContext("com/translationdata/resources/P001_applicationContext.xml");
+		}
+		return appContext.getBean("multiplesOf3And5", P001_MultiplesOf3And5.class);
+	}
+}
