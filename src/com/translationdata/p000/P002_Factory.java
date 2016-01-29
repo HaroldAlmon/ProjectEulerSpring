4f418d3d073f051234e164212a74931b3d4fcac6
haroldalmon <HaroldAlmon@users.noreply.github.com>
2016-01-29 11:06:50 -0600

    Add factory

---
+++ src/com/translationdata/p000/P002_Factory.java
@@ -0,0 +1,21 @@
+package com.translationdata.p000;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public enum P002_Factory {
+	INSTANCE;
+
+	ApplicationContext appContext;
+	
+	public static P002_Factory getFactory() {
+		return INSTANCE;
+	}
+	
+	public P002_EvenFibonacciNumbers getEvenFibonacciNumbers () {
+		if(appContext == null) {
+			appContext = new ClassPathXmlApplicationContext("com/translationdata/resources/P002_applicationContext.xml");
+		}
+		return appContext.getBean("evenFibonacciNumbers ", P002_EvenFibonacciNumbers .class);
+	}
+}
